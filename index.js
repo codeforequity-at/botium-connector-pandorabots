b572145ffdd9101680f05d4fe9185100f2990429
Florian Treml <florian.treml@botium.at>
2021-01-15 13:03:26 +0100

    Added PluginDesc

--- index.js
+++ index.js
@@ -2,5 +2,32 @@ const BotiumConnectorPandorabots = require('./src/connector')
 
 module.exports = {
   PluginVersion: 1,
-  PluginClass: BotiumConnectorPandorabots
+  PluginClass: BotiumConnectorPandorabots,
+  PluginDesc: {
+    name: 'Pandorabots',
+    provider: 'Pandorabots',
+    features: {
+      intentResolution: false
+    },
+    capabilities: [
+      {
+        name: 'PANDORABOTS_BOTKEY',
+        label: 'Pandorabots Bot Key',
+        type: 'string',
+        required: true
+      },
+      {
+        name: 'PANDORABOTS_APIHOST',
+        label: 'Api Host',
+        type: 'url',
+        advanced: true
+      },
+      {
+        name: 'PANDORABOTS_CLIENT_NAME',
+        label: 'Client Name',
+        type: 'string',
+        advanced: true
+      }
+    ]
+  }
 }
